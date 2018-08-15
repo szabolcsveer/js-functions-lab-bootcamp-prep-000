ffa655039e4fb7264e0cff1764fdf961275e978f
Szabolcs Veer <futuremove@gmail.com>
2018-08-15 23:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ happyHolidaysTo(you);
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
-{happyHolidayTo(Independence Day,you);}
+happyHolidayTo(Independence Day,you);
