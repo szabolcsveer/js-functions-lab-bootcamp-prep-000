555ae7c2d55718fddde5dbb81f78f45251abe813
Szabolcs Veer <futuremove@gmail.com>
2018-08-15 22:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+  
+}
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+ }
+happyHolidaysTo(you);
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+{happyHolidayTo(Independence Day,you);}
